0dd8f2f3fc55c9b699d24f8ec3a8ba5806f08387
Sazzad Hossain <sharifh530@gmail.com>
2020-09-08 12:00:17 +0600

    Initialized Learn python repo

---
+++ Beginner/input and print/main.py
@@ -0,0 +1,7 @@
+name = input("Your name:  ")
+address = input("Your address: ")
+phone = input("Your phone: ")
+
+print("Your name: "+ name)
+print("Your address: "+ address)
+print("Your phone: "+ phone)
