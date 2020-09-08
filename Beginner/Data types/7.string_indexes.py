77d290628e64420a61885d7dd3b0d74b1024372f
Sazzad Hossain <sharifh530@gmail.com>
2020-09-08 17:45:22 +0600

    string indexes

---
+++ Beginner/Data types/7.string_indexes.py
@@ -0,0 +1,13 @@
+numbers = "12345678"
+# index     01234567
+
+# [start : stop : stepover]
+
+print(numbers[0:5:2])
+print(numbers[:3])
+print(numbers[1::2])
+
+print(numbers[-2])
+
+# Reverse
+print(numbers[::-1])
