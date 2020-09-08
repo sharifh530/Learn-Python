564345d47f2a843d5300e871f9a63fbea51d3ab3
Sazzad Hossain <sharifh530@gmail.com>
2020-09-08 13:17:27 +0600

    data types - number

---
+++ Beginner/Data types/main.py
@@ -0,0 +1,13 @@
+# Fundamental  data types
+# int - integer number
+# float - floating point number
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+# Classes - Custom Types
+
+# Specialized Data Types
---
+++ Beginner/Data types/numbers.py
@@ -0,0 +1,16 @@
+# int - integer number
+print(type(4))
+
+# float - floating point number
+print(type(4.5))
+
+# Math Function
+# Round
+print(round(4.5))
+# Operator precedence
+# "()" -> "*" -> "* /" -> "+ -"
+
+# Convert decimal to binary(bin)
+print(bin(5))
+# Convert binary to decimal
+print(int("0b101", 2))
