a9f11f884c67afe12590e2fb21af84e3f10a78ba
Sazzad Hossain <sharifh530@gmail.com>
2020-09-08 14:34:04 +0600

    Strings

---
+++ Beginner/Data types/4.strings.py
@@ -0,0 +1,19 @@
+print(type("hello world"))
+
+username = "sazzad"
+print("Username: " + username)
+address = "Dhaka"
+print("Address: "+address)
+
+long_text = '''
+WOW
+0 0
+---
+'''
+print(long_text)
+
+first_name = "Sazzad"
+last_name = "Hossain"
+
+full_name = first_name + " " + last_name
+print(full_name)
---
+++ Beginner/Data types/5.escape_sequence.py
@@ -0,0 +1,12 @@
+# whatever comes after "\" it will be a string
+
+weather = "Its a \"kind of sunny\" weather "
+
+print(weather)
+
+# whatever comes after "\t" it create a tab space and "\n" creates a new line
+
+
+weather1 = "\t Its a \"kind of sunny\" weather  \n hope you have a good day"
+
+print(weather1)
