4441816347310bb3decfe4151f5810f3f4d76ef7
Sazzad Hossain <sharifh530@gmail.com>
2020-09-10 02:23:18 +0600

    logical operators

---
+++ Advance/Logic/3.logical_operator.py
@@ -0,0 +1,18 @@
+is_magician = False
+is_expert = True
+
+
+if is_magician and is_expert:
+    print("you are a master magician")
+elif is_magician and not is_expert:
+    print("at least you're getting there")
+elif not is_magician:
+    print("you need magic powers")
+
+# Logical Operators
+
+print(1 > 2)
+print(1 == 1)
+print(1 <= 0)
+print(5 <= 10)
+print(1 is 1)
