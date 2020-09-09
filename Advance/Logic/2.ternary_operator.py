40d4ae4022f9f0025e00cc702fed1406747aa141
Sazzad Hossain <sharifh530@gmail.com>
2020-09-10 01:42:27 +0600

    ternary operator

---
+++ Advance/Logic/2.ternary_operator.py
@@ -0,0 +1,9 @@
+# Ternary operator
+
+# condition_if_true if condition else condition_if_false
+
+is_friend = False
+
+can_message = "message allowed" if is_friend else "Message is not allowed"
+
+print(can_message)
