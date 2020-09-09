818a10236bd0aedea40cc9bc41c489efae85cd76
Sazzad Hossain <sharifh530@gmail.com>
2020-09-10 01:29:13 +0600

    conditional logic

---
+++ Advance/Logic/1.conditional_logic.py
@@ -0,0 +1,13 @@
+# Conditional Logic
+
+is_old = True
+is_licenced = True
+
+if is_old and is_licenced:
+    print("You're old enough to Drive")
+
+elif is_licenced:
+    print("you have your licence and allowed to drive")
+
+else:
+    print("You're young and not allowed to drive")
