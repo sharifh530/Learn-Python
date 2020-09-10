5f916956fa9eabe47836e4b60e4189052b0ce8c4
Sazzad Hossain <sharifh530@gmail.com>
2020-09-10 22:00:56 +0600

    exercise 2

---
+++ Advance/Loops/4.find_duplicates.py
@@ -0,0 +1,14 @@
+# Find the duplicates of this array
+
+
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+
+print(duplicates)
