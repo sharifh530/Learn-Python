63d5e72a7b366868e2f22d17607741e36bb29a88
Sazzad Hossain <sharifh530@gmail.com>
2020-09-10 21:52:36 +0600

    exercise 1

---
+++ Advance/Loops/3.print_picture.py
@@ -0,0 +1,19 @@
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+star = "*"
+empty = ""
+
+for row in picture:
+    for pixel in row:
+        if(pixel == 1):
+            print(star, end=empty)
+        else:
+            print(" ", end=empty)
+    print(empty)
